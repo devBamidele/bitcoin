5c576f24ec1fa8a1b652f40580b42566cf914b58
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-07-06 16:16:33 +0100

    I ran out of requests

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -32,18 +32,15 @@ const List<String> currenciesList = [
   'NGN'
 ];
 
+// Todo: Uncomment the two lines here
 const List<String> cryptoList = [
   'BTC',
-  'ETH',
-  'LTC',
+  // 'ETH',
+  // 'LTC',
 ];
 
 class CoinData {
   CoinData();
-
-  /// Sample request
-  /// https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=E736773C-754D-4E8B-861D-70F96C1F5604
-  /// https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=E736773C-754D-4E8B-861D-70F96C1F5604#
 
   /// Fetch the information from the server and return it as a json String
   Future<dynamic> fetchData(String url) async {
@@ -67,6 +64,7 @@ class CoinData {
     }
   }
 
+  // todo: You need to interpolate 'BTC' to reduce DRY
   getBitcoin(String currency) async {
     var url = '${openWeatherMapUrl}BTC/$currency?apikey=$apiKey1';
     return await fetchData(url);
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,10 +6,6 @@ import 'package:bitcoin/coin_data.dart';
 import 'package:bitcoin/recyclable_card.dart';
 import 'dart:io' show Platform;
 
-// BTC
-// ETH
-// LTE
-
 class PriceScreen extends StatefulWidget {
   const PriceScreen({Key? key}) : super(key: key);
 
@@ -20,6 +16,7 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
   int? btcrate, ethrate, ltcrate;
+  List<int> exchange = [];
 
   getAndroidPicker() {
     List<DropdownMenuItem<String>> items =
@@ -63,6 +60,7 @@ class _PriceScreenState extends State<PriceScreen> {
         setState(() {
           selectedCurrency = currenciesList[selectedPicker];
           getData();
+          log(exchange.length.toString());
         });
       },
       children: myItems,
@@ -75,19 +73,25 @@ class _PriceScreenState extends State<PriceScreen> {
     getData();
   }
 
+  // Todo: The getData needs to receive responses and store it in an array
   void getData() async {
     CoinData coinData = CoinData();
 
-    dynamic bitcoin = await coinData.getBitcoin(selectedCurrency);
+    for (int i = 0; i < cryptoList.length; i++) {
+      dynamic bitcoin = await coinData.getBitcoin(selectedCurrency);
 
-    if (bitcoin != null) {
-      if (bitcoin == 'Bad response') {
-        btcrate = null;
-      } else {
-        double result = bitcoin['rate'];
-        setState(() {
-          btcrate = result.toInt();
-        });
+      if (bitcoin != null) {
+        if (bitcoin == 'Bad response') {
+          btcrate = null; // This line is not necessary btw
+        } else {
+          double result = bitcoin['rate'];
+          exchange.add(result.toInt());
+          setState(
+            () {
+              btcrate = exchange[i];
+            },
+          );
+        }
       }
     }
   }
@@ -108,18 +112,19 @@ class _PriceScreenState extends State<PriceScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                // todo: Add the remaining cards and set the amount: to be arraylist[0] or so
                 MyCard(
                   selectedCurrency: selectedCurrency,
                   bitCoinCurrency: 'BTC',
                   amount: btcrate.toString(),
                 ),
-                const SizedBox(height: 30),
+                const SizedBox(height: 15),
                 MyCard(
                   selectedCurrency: selectedCurrency,
                   bitCoinCurrency: 'BTC',
                   amount: btcrate.toString(),
                 ),
-                const SizedBox(height: 30),
+                const SizedBox(height: 15),
                 MyCard(
                   selectedCurrency: selectedCurrency,
                   bitCoinCurrency: 'BTC',
